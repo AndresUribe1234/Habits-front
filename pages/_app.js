268059c048a8711c2b436844e0c8c8a4b0afd444
AndresUribe1234 <anduri1997@gmail.com>
2023-03-17 22:34:43 -0500

    vercel analytic component inserted

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "@/components/layout/Layout";
 import { AuthContextProvider } from "@/store/auth-context";
 import { ThemeProvider } from "@mui/material";
 import theme from "./../styles/muiTheme";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
       <AuthContextProvider>
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </AuthContextProvider>
     </ThemeProvider>
