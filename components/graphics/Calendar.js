ee68b25f19b2abccc4ac154060178469a8371e3d
AndresUribe1234 <anduri1997@gmail.com>
2023-06-15 12:34:03 -0500

    halfway changes to calendar component

--- components/graphics/Calendar.js
+++ components/graphics/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import moment from "moment";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
@@ -9,184 +9,193 @@ import classes from "./../../styles/Calendar.module.scss";
 
 function Calendar(props) {
   const [currentDate, setCurrentDate] = useState(moment());
+  const [calendar, setCalendar] = useState([]);
+  const [data, setData] = useState([]);
+  const [dataDateValue, setDataDateValue] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalData, setModalData] = useState({});
 
-  const { data } = props;
+  useEffect(() => {
+    // Set data from props
+    setData(props.data);
 
-  const dateAsNumSecUTCFromData = data.map((ele) =>
-    new Date(ele.registrationFinalDate).getTime()
-  );
+    // Get array of dates from data for when rendering a date knowing if theres an event
+    const timeSinceUTC = props.data.map((ele) => {
+      const momentDate = moment
+        .utc(ele.registrationFinalDate)
+        .tz("America/Bogota")
+        .format("YYYY-MM-DD");
 
-  const firstDayOfMonthWeekDayIndex = currentDate.startOf("month").day();
+      return new Date(momentDate).getTime();
+    });
+    setDataDateValue(timeSinceUTC);
+  }, [props.data]);
 
-  // 1)Define object where month information will live on
-  const calenderObject = {
-    weekOne: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-    weekTwo: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-    weekThree: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-    weekFour: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-    weekFive: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-    weekSix: { dateValueSecUTCForCalendar: [], valueForCalendar: [] },
-  };
+  useEffect(() => {
+    const calculateCalendar = () => {
+      // Day index a month starts with. Example:Sunday=0,monday=1, tuesday=2,etc.
+      const beginningOfMonthDayIndex = currentDate.startOf("month").day();
 
-  const keysOfCalenderObject = [
-    "weekOne",
-    "weekTwo",
-    "weekThree",
-    "weekFour",
-    "weekFive",
-    "weekSix",
-  ];
+      // Calendar array consisting of 6 possible weeks a month can have
+      let calendar = Array.from({ length: 6 }, () => ({
+        valueHtmlCalendar: [],
+        dateValueSecondsFromUTC: [],
+        dateValueString: [],
+      }));
 
-  // 2)Adjust first weekdays values depending on which day of the week the month starts
-  for (let i = 0; i < firstDayOfMonthWeekDayIndex; i++) {
-    calenderObject.weekOne.valueForCalendar.push("");
-    calenderObject.weekOne.dateValueSecUTCForCalendar.push("");
-  }
+      // Populate first week with empty spaces until first day of month (a month can start on a Tuesday).
+      for (let i = 0; i < beginningOfMonthDayIndex; i++) {
+        calendar[0].valueHtmlCalendar.push("");
+        calendar[0].dateValueSecondsFromUTC.push("");
+        calendar[0].dateValueString.push("");
+      }
 
-  // 3)Fill calender object
-  const daysInMonth = currentDate.daysInMonth();
-  let weekCounter = 1;
-  let currentDateCounter = currentDate.clone();
+      // Get number of days in a month in order to populate array with corresponding data
+      const daysInMonth = currentDate.daysInMonth();
+      let weekCounter = 0;
+      // Make a copy of current date in order to avoid changing unintentionally
+      let currentDateCounter = currentDate.clone();
 
-  for (let i = 1; i <= daysInMonth; i++) {
-    // Assign to specific key in calender object depending of week counter
-    calenderObject[
-      `${keysOfCalenderObject[weekCounter - 1]}`
-    ].valueForCalendar.push(i);
+      // Iterate over all days of month in order to populate calendar array with information needed
+      for (let i = 1; i <= daysInMonth; i++) {
+        calendar[weekCounter].valueHtmlCalendar.push(i);
+        calendar[weekCounter].dateValueSecondsFromUTC.push(
+          new Date(currentDateCounter.format("YYYY-MM-DD")).getTime()
+        );
+        calendar[weekCounter].dateValueString.push(
+          String(currentDateCounter.format("YYYY-MM-DD"))
+        );
 
-    calenderObject[
-      `${keysOfCalenderObject[weekCounter - 1]}`
-    ].dateValueSecUTCForCalendar.push(
-      new Date(currentDateCounter.format("YYYY-MM-DD")).getTime()
+        // Once day index reaches end of week jump to next week
+        if (currentDateCounter.day() === 6) {
+          weekCounter = weekCounter + 1;
+        }
+        // Add day to calendar day counter
+        currentDateCounter.add(1, "d");
+      }
+      // Return calendar object as value
+      return calendar;
+    };
+
+    setCalendar(calculateCalendar());
+  }, [currentDate]);
+
+  const calendarHandler = (typeOfOperation, unit) => {
+    setCurrentDate(
+      typeOfOperation === "add"
+        ? moment(currentDate).add(1, unit)
+        : moment(currentDate).subtract(1, unit)
     );
-
-    // When current day index is 6 which mean last day of week change week by adding one
-    if (currentDateCounter.day() === 6) {
-      weekCounter = weekCounter + 1;
-    }
-    currentDateCounter.add(1, "d");
-  }
-
-  const handlePrevMonth = () => {
-    setCurrentDate(moment(currentDate).subtract(1, "month"));
   };
 
-  const handleNextMonth = () => {
-    setCurrentDate(moment(currentDate).add(1, "month"));
+  const interactCalendarHandler = (data) => {
+    setModalData(data);
+    setModalView(true);
   };
 
-  const handlePrevYear = () => {
-    setCurrentDate(moment(currentDate).subtract(1, "year"));
-  };
-
-  const handleNextYear = () => {
-    setCurrentDate(moment(currentDate).add(1, "year"));
-  };
-
-  const modalHandlerOpenStatus = (event) => {
-    const registrationId = event.target.dataset.id;
-    const registrationIndex = data
-      .map((ele) => ele._id)
-      .indexOf(registrationId);
-
-    setModalData(data[registrationIndex]);
-    setModalVisible(true);
-  };
-
-  const modalHandlerCloseStatus = (event) => {
-    setModalVisible(false);
-  };
-
-  return (
-    <div className={classes["calendar-container"]}>
-      <div className={classes["change-date-container"]}>
-        <div className={classes["change-date-btn-container"]}>
-          <div className={classes["change-date-btn"]} onClick={handlePrevYear}>
-            <KeyboardDoubleArrowLeftIcon />
-          </div>
-          <div className={classes["change-date-btn"]} onClick={handlePrevMonth}>
-            <KeyboardArrowLeftIcon />
-          </div>
+  const tableDateFunctionalityHTML = (
+    <div className={classes["change_date_container"]}>
+      <div className={classes["change_date_btn_container"]}>
+        <div
+          className={classes["change_date_btn"]}
+          onClick={calendarHandler.bind(this, "subtract", "year")}
+        >
+          <KeyboardDoubleArrowLeftIcon />
         </div>
-        <h2 className={classes["current-date"]}>
-          {currentDate.format("MMMM YYYY")}
-        </h2>
-        <div className={classes["change-date-btn-container"]}>
-          <div className={classes["change-date-btn"]} onClick={handleNextMonth}>
-            <KeyboardArrowRightIcon />
-          </div>
-          <div className={classes["change-date-btn"]} onClick={handleNextYear}>
-            <KeyboardDoubleArrowRightIcon />
-          </div>
+        <div
+          className={classes["change_date_btn"]}
+          onClick={calendarHandler.bind(this, "subtract", "month")}
+        >
+          <KeyboardArrowLeftIcon />
         </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {keysOfCalenderObject.map((ele, index) => {
-            // 1)Declare empty body of tr tag
+      <h2 className={classes["current_date"]}>
+        {currentDate.locale("es").format("MMMM YYYY")}
+      </h2>
+      <div className={classes["change_date_btn_container"]}>
+        <div
+          className={classes["change_date_btn"]}
+          onClick={calendarHandler.bind(this, "add", "month")}
+        >
+          <KeyboardArrowRightIcon />
+        </div>
+        <div
+          className={classes["change_date_btn"]}
+          onClick={calendarHandler.bind(this, "add", "year")}
+        >
+          <KeyboardDoubleArrowRightIcon />
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className={classes.calendar_container}>
+      {tableDateFunctionalityHTML}
+      <div className={classes.calendar_table_container}>
+        <div className={classes.row_container}>
+          <div className={classes.row_cell}>Dom</div>
+          <div className={classes.row_cell}>Lun</div>
+          <div className={classes.row_cell}>Mar</div>
+          <div className={classes.row_cell}>Mie</div>
+          <div className={classes.row_cell}>Jue</div>
+          <div className={classes.row_cell}>Vie</div>
+          <div className={classes.row_cell}>Sab</div>
+        </div>
+        <div className={classes.calendar_body}>
+          {calendar.map((ele, weekIndex) => {
+            //Declare empty body of tr tag
             const rowsBody = [];
-            // 2)Loop through all data of correspodning week. "In" returns index in for loop.
-            for (let value in calenderObject[`${ele}`].valueForCalendar) {
-              // 3)Get date in n position of dateValue array
-              const dateValue =
-                calenderObject[`${ele}`].dateValueSecUTCForCalendar[value];
-              // 4)If date exists in registrations assign information to tag
+            //Loop through all data of corresponding week. "In" returns index in for loop. "Of" returns value in for loop.
+            for (let index in ele.valueHtmlCalendar) {
+              const day = ele.valueHtmlCalendar[index];
+              //   Add html value for each day in order to construct array od <td/> tags that will build <tr/> tag
 
-              if (dateAsNumSecUTCFromData.includes(dateValue)) {
-                const indexInDataArray =
-                  dateAsNumSecUTCFromData.indexOf(dateValue);
-                const id = data[indexInDataArray]._id;
-                const completionStatus = data[indexInDataArray].completionStatus
-                  .toLowerCase()
-                  .split(" ")
-                  .join("");
+              //  Html passed if date appear in data
+              console.log(
+                dataDateValue.includes(ele.dateValueSecondsFromUTC[index])
+              );
 
+              if (dataDateValue.includes(ele.dateValueSecondsFromUTC[index])) {
                 rowsBody.push(
-                  <td key={value}>
-                    <div
-                      className={[
-                        classes[`${completionStatus}`],
-                        classes["data-logged"],
-                      ].join(" ")}
-                      onClick={modalHandlerOpenStatus}
-                      data-id={id}
-                    >
-                      {calenderObject[`${ele}`].valueForCalendar[value]}
-                    </div>
-                  </td>
+                  <div
+                    key={Math.random()}
+                    data-week={index + 1}
+                    data-day={day}
+                    className={classes.row_cell}
+                  >
+                    <div>{day}</div>
+                  </div>
                 );
               }
-              // 5)If date does not exist simply apply date
-              if (!dateAsNumSecUTCFromData.includes(dateValue)) {
+
+              // Html if theres nof information for this date
+              if (!dataDateValue.includes(ele.dateValueSecondsFromUTC[index])) {
                 rowsBody.push(
-                  <td key={value}>
-                    {calenderObject[`${ele}`].valueForCalendar[value]}
-                  </td>
+                  <div
+                    key={Math.random()}
+                    data-week={index + 1}
+                    data-day={day}
+                    className={classes.row_cell}
+                  >
+                    <div>{day}</div>
+                  </div>
                 );
               }
             }
-
+            // Return one table row for each week of the calendar array
             return (
-              <tr data-week={index + 1} key={index}>
+              <div
+                data-week={weekIndex}
+                key={Math.random()}
+                className={classes.row_container}
+              >
                 {rowsBody}
-              </tr>
+              </div>
             );
           })}
-        </tbody>
-      </table>
+        </div>
+      </div>
       {modalVisible && (
         <ModalMuiHabitInformation
           status={modalVisible}
